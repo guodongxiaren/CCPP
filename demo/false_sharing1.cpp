422f38a653659494da52927051bded02137ae8b6
guodongxiaren <879231132@qq.com>
2021-03-26 21:47:24 +0800

    yes

--- demo/false_sharing1.cpp
+++ demo/false_sharing1.cpp
@@ -3,19 +3,21 @@
 #include <iostream>
 #include <chrono>
 using namespace std;
-void foo(const vector<int> v, vector<long> sum, int id) {
+const int N = 10000; // vector v 大小
+const int M = 2;     // vector sum 大小
+void foo(const vector<int>& v, vector<long> sum, int id) {
     for (int i = 0; i < v.size(); ++i) {
-        if (i%2 == id) {
+        if (i%M == id) {
             sum[id] += i;
         }
     }
     cout << "sum " << id << " = " << sum[id] << endl;
 }
 
-void foo2(const vector<int> v, vector<long> sum, int id) {
+void foo2(const vector<int>& v, vector<long> sum, int id) {
     long s = 0;
     for (int i = 0; i < v.size(); ++i) {
-        if (i%2 == id) {
+        if (i%M == id) {
             s += i;
         }
     }
@@ -23,35 +25,31 @@ void foo2(const vector<int> v, vector<long> sum, int id) {
     cout << "sum " << id << " = " << sum[id] << endl;
 }
 int main () {
-    {
-        vector<int> v;
-        for (int i = 0; i < 100000; ++i) {
-            v.push_back(i);
-        }
+    vector<int> v;
+    for (int i = 0; i < N; ++i) {
+        v.push_back(i);
+    }
 
+    {   // 代码块 1
         auto start = chrono::steady_clock::now();
-        vector<long> sum = {0, 0};
-        thread t1(foo, v, sum, 0);
-        thread t2(foo, v, sum, 1);
-        t1.join();
-        t2.join();
-
+        vector<long> sum(M, 0);
+        vector<thread> td;
+        for (int i = 0; i < M; ++i) {
+            td.emplace_back(foo, v, sum, i);
+        }
+        for (int i = 0; i < M; ++i) {
+            td[i].join();
+        }
         auto end = chrono::steady_clock::now();
         cout<< "block 1 cost:" << chrono::duration_cast<chrono::microseconds>(end - start).count()<<endl;
     }
     cout << "----------" << endl;
-    {
-        vector<int> v;
-        for (int i = 0; i < 100000; ++i) {
-            v.push_back(i);
-        }
-
+    {   // 代码块2
         auto start = chrono::steady_clock::now();
-        vector<long> sum = {0, 0};
-
-        foo(v, sum, 0);
-        foo(v, sum, 1);
-
+        vector<long> sum(M, 0);
+        for (int i = 0; i < M; ++i) {
+            foo(v, sum, i);
+        }
         auto end = chrono::steady_clock::now();
         cout<< "block 2 cost:" << chrono::duration_cast<chrono::microseconds>(end - start).count()<<endl;
     }
